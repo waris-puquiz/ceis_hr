f057c690b8abc65b9c82d578fdde268b084859bd
waris-puquiz <wariscruzpuquiz@gmail.com>
2023-03-27 09:48:07 +0800

    main-page navbar

--- mainpage.php
+++ mainpage.php
@@ -16,26 +16,46 @@
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
 <link rel="stylesheet" href="https://kit.fontawesome.com/471326bbab.css" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-<link rel="stylesheet" href="resources/css/mainpage.css"  /> 
+<link rel="stylesheet" href="resources/css/main-page.css"  /> 
 <script src="js/app.js"></script>  
 </head>
 <body class="intro">
-<nav class="navbar navbar-expand-lg navbar-light ">
-<div class="logo-header">
-      <div class="logo_image text-start">
-        <a><img class="ceis_logo " src="resources/images/logo-ceis.png"/></a>
-      </div>
-</div>  
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
+
+<!-- Image and text -->
+<main class="text-center">
+<nav class="navbar navbar text-center" style="--count: 4;">
+  <a class="navbar-brand" href="#">
+    <img src="resources/images/logo-ceis.png" width="300" height="200" class="d-inline-block align-top" alt="">
+  </a>
+  <ul>
+        <li style="--index: 1;">
+          <a class="icn" href="#" target="_blank" rel="noreferrer noopener">
+            <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+  </svg>
+  Admin</span>
+          </a>
+        </li>
+        <li style="--index: 2;">
+          <a  class="icn"href="" target="_blank" rel="noreferrer noopener">
+            <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+              Add</span>
+          </a>
+        </li>
+        <li style="--index: 3;">
+          <a  class="icn"href="https://front-end.social/jhey" target="_blank" rel="noreferrer noopener">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+              Logout
+            </span>
+          </a>
+        </li>
+      
+      </ul> 
 </nav>
-<main>
+</main>
 
   
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
